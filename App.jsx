8e63cdfbf371d80c9d49db6f0fd6a99c57496a1c
samuel <sammykaris48@gmail.com>
2023-05-30 07:29:22 +0300

    added

--- App.jsx
+++ App.jsx
@@ -3,51 +3,75 @@ import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
 import Split from "react-split"
 import { nanoid } from "nanoid"
-import { onSnapshot, addDoc, doc, deleteDoc, setDoc } from "firebase/firestore"
-import { notesCollection } from "./firebase"
+import {
+    onSnapshot,
+    addDoc,
+    doc,
+    deleteDoc,
+    setDoc
+} from "firebase/firestore"
+import { notesCollection, db } from "./firebase"
 
 export default function App() {
-    const [notes, setNotes] = React.useState( [] )
+    const [notes, setNotes] = React.useState([])
     const [currentNoteId, setCurrentNoteId] = React.useState("")
+    const [tempNoteText, setTempNoteText] = React.useState("")
     
-    const currentNote = 
-        notes.find(note => note.id === currentNoteId) 
+    const currentNote =
+        notes.find(note => note.id === currentNoteId)
         || notes[0]
-
-        const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt)
-
-        React.useEffect(() => {
-            const unsubscribe = onSnapshot(notesCollection, function(snapshot) {
-                // Sync up our local notes array with the snapshot data
-                const notesArr = snapshot.docs.map(doc => ({
-                    ...doc.data(),
-                    id: doc.id
-                }))
-                setNotes(notesArr)
-            })
-            return unsubscribe
-        }, [])
-
-        React.useEffect(() => {
-            if (!currentNoteId) {
-                setCurrentNoteId(notes[0]?.id)
-            }
-        }, [notes])
-
-        async function createNewNote() {
-            const newNote = {
-                body: "# Type your markdown note's title here",
-                createdAt: Date.now(),
-                updatedAt: Date.now()
-            }
-            const newNoteRef = await addDoc(notesCollection, newNote)
-            setCurrentNoteId(newNoteRef.id)
-        }
-
     
+    const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt)
+
+    React.useEffect(() => {
+        const unsubscribe = onSnapshot(notesCollection, function (snapshot) {
+            const notesArr = snapshot.docs.map(doc => ({
+                ...doc.data(),
+                id: doc.id
+            }))
+            setNotes(notesArr)
+        })
+        return unsubscribe
+    }, [])
+    
+    React.useEffect(() => {
+        if (!currentNoteId) {
+            setCurrentNoteId(notes[0]?.id)
+        }
+    }, [notes])
+    
+    React.useEffect(() => {
+        if (currentNote) {
+            setTempNoteText(currentNote.body)
+        }
+    }, [currentNote])
+    
+    React.useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if (tempNoteText !== currentNote.body) {
+                updateNote(tempNoteText)
+            }
+        }, 500)
+        return () => clearTimeout(timeoutId)
+    }, [tempNoteText])
+
+    async function createNewNote() {
+        const newNote = {
+            body: "# Type your markdown note's title here",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        }
+        const newNoteRef = await addDoc(notesCollection, newNote)
+        setCurrentNoteId(newNoteRef.id)
+    }
+
     async function updateNote(text) {
         const docRef = doc(db, "notes", currentNoteId)
-        await setDoc(docRef, { body: text, updatedAt: Date.now() }, { merge: true })
+        await setDoc(
+            docRef, 
+            { body: text, updatedAt: Date.now() }, 
+            { merge: true }
+        )
     }
 
     async function deleteNote(noteId) {
@@ -72,14 +96,10 @@ export default function App() {
                             newNote={createNewNote}
                             deleteNote={deleteNote}
                         />
-                        {
-                            currentNoteId &&
-                            notes.length > 0 &&
-                            <Editor
-                                currentNote={currentNote}
-                                updateNote={updateNote}
-                            />
-                        }
+                        <Editor
+                            tempNoteText={tempNoteText}
+                            setTempNoteText={setTempNoteText}
+                        />
                     </Split>
                     :
                     <div className="no-notes">
--- components/Editor.jsx
+++ components/Editor.jsx
@@ -3,7 +3,7 @@ import ReactMde from "react-mde"
 //const ReactMde = R.default
 import Showdown from "showdown"
 
-export default function Editor({ currentNote, updateNote }) {
+export default function Editor({ tempNoteText, setTempNoteText }) {
     const [selectedTab, setSelectedTab] = React.useState("write")
 
     const converter = new Showdown.Converter({
@@ -11,13 +11,13 @@ export default function Editor({ currentNote, updateNote }) {
         simplifiedAutoLink: true,
         strikethrough: true,
         tasklists: true,
-    })  
+    })
 
     return (
         <section className="pane editor">
             <ReactMde
-                value={currentNote?.body}
-                onChange={updateNote}
+                value={tempNoteText}
+                onChange={setTempNoteText}
                 selectedTab={selectedTab}
                 onTabChange={setSelectedTab}
                 generateMarkdownPreview={(markdown) =>
@@ -29,3 +29,4 @@ export default function Editor({ currentNote, updateNote }) {
         </section>
     )
 }
+
